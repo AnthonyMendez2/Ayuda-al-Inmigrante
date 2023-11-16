369f6719dd90ac1aa4691426e2980c16d34a1550
Anthony <soyanthonymendez@gmail.com>
2023-11-16 08:17:58 -0300

    condicionado el formulario

---
+++ app.js
@@ -0,0 +1,18 @@
+function textoInput(event){
+    let texto = event.keyCode;
+    
+    if((texto > 64 && texto < 91) || (texto === 8) || (texto === 32) || (texto ===37) || (texto ===39) || (texto ===16) ||(texto ===20) || (texto ===17) || (texto ===46) || (texto ===9)){ 
+        return true;
+    }else{
+        return false;
+    }
+}
+
+function numInput(event){
+    let num = event.keyCode;
+    if((num > 47 && num < 58) || (num > 95 && num < 106) || (num === 107) || (num === 37) || (num === 39) || (num === 8) || (num === 17) || (num === 16) || (num === 46)){
+        return true;
+    }else{
+        return false;
+    }
+}
---
+++ componentes.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAnbyY1OxYuAgPs026_KVQHEX1CDejiFyU",
+  authDomain: "registro-ayuda-inmigrante.firebaseapp.com",
+  projectId: "registro-ayuda-inmigrante",
+  storageBucket: "registro-ayuda-inmigrante.appspot.com",
+  messagingSenderId: "1060186602640",
+  appId: "1:1060186602640:web:ed9ad021ececc5e8a22c15"
+};
+
+// Initialize Firebase
+const appfirebase = initializeApp(firebaseConfig);
+export default appfirebase;
