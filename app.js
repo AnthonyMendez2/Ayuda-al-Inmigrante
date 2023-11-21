11c6d17adfcd843ebd0d0275aa7a16c2de22e6b0
AixaHernandezv <aixahernandezv@gmail.com>
2023-11-21 15:40:43 -0300

    ajuste imagen desktop & mobile

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const callBtn = document.querySelector('#callBtn');
 callBtn.addEventListener("click",openWhatsApp);
 
+const callBtnMobile = document.querySelector('#callBtnMobile');
+callBtnMobile.addEventListener("click",openWhatsApp);
+
+
+
 function openWhatsApp() {
     const phone = '+56965948316';
     const message = 'Hola necesito ayuda';
